657ac0aa72560c4725333f2e76cb2468b965afca
AlexisComix <thecomixguy@hotmail.com>
2023-02-22 14:01:41 +0000

    Start to add support for better datatypes

--- src/user.rs
+++ src/user.rs
@@ -1,21 +1,14 @@
 #![allow(dead_code)]
 
+pub use uuid::{uuid, Uuid};
+
+pub use email_address::*;
+pub use std::str::FromStr;  // Use ::from_str() to gen email
+
 #[derive(Debug)]
 pub struct User {
-    id: i32,
+    UUID: Uuid,
+    email: EmailAddress,
     first_name: String,
     last_name: String,
-    email: String,
-    password: String,   
-}
-impl User {
-    pub fn new(id: i32, first_name: String, last_name: String, email: String, password: String,) -> User {
-        User {
-            id: id,
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password:password
-        }
-    }
 }
