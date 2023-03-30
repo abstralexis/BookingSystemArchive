92c63a27475ec5289adffc41fd3aae727537b568
AlexisComix <Thecomixguy@hotmail.com>
2023-03-30 13:29:19 +0100

    Update toolchain and modularise

---
+++ server/src/postgres_commands.rs
@@ -0,0 +1,34 @@
+use http::{Request, Response, StatusCode};
+use uuid::Uuid;
+use chrono::prelude::*;
+use email_address::*;
+use tokio;
+use postgres::{Client, Error, NoTls};
+
+async fn connect_and_initialise() -> Result<Client, Error> {
+    let mut client: Client = Client::connect(
+        "postgresql://postgres:AlexisDB@localhost/bookingsystem",
+        NoTls,
+    )?;
+
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            uuid UUID UNIQUE NOT NULL,
+            email VARCHAR UNIQUE NOT NULL,
+            first_name VARCHAR NOT NULL,
+            last_name VARCHAR NOT NULL,
+            hashed_password VARCHAR NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS bookings (
+            uuid UUID NOT NULL,
+            booker_id UUID NOT NULL,
+            start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+            end_time TIMESTAMP WITH TIME ZONE NOT NULL
+        );
+    ",
+    )?;
+
+    Ok(client)
+}
